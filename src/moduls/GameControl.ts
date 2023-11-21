3f395c530a3a961cf7cbc360493929975e8cbfc8
Jun <3010977181@qq.com>
2023-11-21 20:25:56 +0800

    1.0

--- src/moduls/GameControl.ts
+++ src/moduls/GameControl.ts
@@ -6,13 +6,73 @@ class GameControl {
     food: Food
     scorePanel: ScorePanel
     snake: Snake
+    // 按键的方向
+    direction: string = ''
+    // 游戏的开始
+    isLive = true
     constructor() {
         this.food = new Food()
         this.scorePanel = new ScorePanel()
         this.snake = new Snake()
+        this.init()
     }
-
-
+    // 游戏初始化方法
+    init() {
+        document.addEventListener('keydown', this.keydownHandler)
+        this.run()
+    }
+    // 检测键盘响应的函数
+    keydownHandler = (event: KeyboardEvent) => {
+        this.direction = event.key
+    }
+    // 改变蛇的位置
+    run() {
+        // 获得蛇头的位置
+        let X = this.snake.X
+        let Y = this.snake.Y
+        switch (this.direction) {
+            case "ArrowUp":
+            case "Up":
+            case "W":
+            case "w":
+                Y -= 10
+                break;
+            case "ArrowDown":
+            case "Down":
+            case "S":
+            case "s":
+                Y += 10
+                break;
+            case "ArrowLeft":
+            case "Left":
+            case "A":
+            case "a":
+                X -= 10
+                break;
+            case "ArrowRight":
+            case "Right":
+            case "D":
+            case "d":
+                X += 10
+                break;
+        }
+        try {
+            this.snake.X = X
+            this.snake.Y = Y
+        } catch (e) {
+            alert(e)
+            this.isLive = false
+        }
+        this.isLive && setTimeout(this.run.bind(this), 300 - (this.scorePanel.level - 1) * 30);
+        this.checkEat(X, Y)
+    }
+    // 判断蛇是否吃到食物
+    checkEat(x: number, y: number) {
+        if (x === this.food.X && y === this.food.Y) {
+            this.food.change()
+            this.snake.addBody()
+            this.scorePanel.addScore()
+        }
+    }
 }
-
 export default GameControl
--- src/moduls/Snake.ts
+++ src/moduls/Snake.ts
@@ -21,15 +21,49 @@ class Snake {
     }
     // 设置蛇头x轴坐标
     set X(value: number) {
+        if (this.X === value) return
+        if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetLeft === value) {
+            if (value > this.X) value = this.X - 10
+            else value = this.X + 10
+        }
+
+        if (value < 0 || value > 290) throw new Error("蛇撞墙了,游戏结束")
+        this.moveBody()
         this.head.style.left = value + 'px'
+        this.checkHeadBody()
     }
     // 设置蛇头y轴坐标
     set Y(value: number) {
+        if (this.Y === value) return
+        if (this.bodies[1] && (this.bodies[1] as HTMLElement).offsetTop === value) {
+            if (value > this.Y) value = this.Y - 10
+            else value = this.Y + 10
+        }
+        if (value < 0 || value > 290) throw new Error("蛇撞墙了,游戏结束")
+        this.moveBody()
         this.head.style.top = value + 'px'
+        this.checkHeadBody()
     }
     // 蛇增加身体的方法
     addBody() {
         this.element.insertAdjacentHTML('beforeend', '<div></div>')
+    }
+    // 蛇身体移动的方法
+    moveBody() {
+        for (let i = this.bodies.length - 1; i > 0; i--) {
+            let X = (this.bodies[i - 1] as HTMLElement).offsetLeft;
+            let Y = (this.bodies[i - 1] as HTMLElement).offsetTop;
+            (this.bodies[i] as HTMLElement).style.left = X + 'px';
+            (this.bodies[i] as HTMLElement).style.top = Y + 'px';
+        }
+    }
+    // 检查蛇头是否与蛇身相撞
+    checkHeadBody() {
+        for (let i = 1; i < this.bodies.length; i++) {
+            if (this.X === (this.bodies[i] as HTMLElement).offsetLeft && this.Y === (this.bodies[i] as HTMLElement).offsetTop) {
+                throw new Error("撞到自己了！")
+            }
+        }
     }
 }
 
